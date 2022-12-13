8af018270ef9160e367a6f62ced7b5d31c6fde56
Dr Z <1869730+sazamore@users.noreply.github.com>
2022-12-13 11:06:30 -0700

    for timing tasks - now has override & reset

--- pgTimer.py
+++ pgTimer.py
@@ -1,6 +1,9 @@
 '''
-AUTHOR: Suibi
+AUTHOR: Suibi, Dr. Z
 11/9/2022
+
+12/13/2022 - Updated to make get_Time(). No longer called with display to help stop clocks.
+Added font parameter to Timer init to allow ease of changing fonts.
 
 Uses pygame Timer and font objects to make a countup (stopwatch) and countdown timer class.
 
@@ -20,22 +23,26 @@ import pygame
 pygame.init()
 
 class Timer:
-  def __init__(self,countDir=True,time=60,text='0'):
+  def __init__(self,countDir=True,time=60,text='0',font='Consolas'):
       self.totalTime = time
       self.countDir = countDir # if False, then clock counts up
-      self.setToZero(time) # resets clock based on self.countDown
+      self.reset(time) # resets clock based on self.countDown
 
       # text settings
       self.fontSize=30
-      self.font = pygame.font.SysFont('Consolas',
+      self.font = pygame.font.SysFont(font,
                                       self.fontSize)
       self.text = text # string to print
+      self.manual = False # for overRiding clock time.
     
+  def getTime(self):
+      '''Gets the current time. For se in countdown and countup.'''
+      return=(pygame.time.get_ticks()-self.start_ticks)/1000
+
   def countUp(self):
       '''calculates time passed and returns time in
       seconds (int).'''
-      seconds=(pygame.time.get_ticks()-self.start_ticks)/1000
-      self.currTime = int(seconds)
+      self.currTime = int(getTime())
       return self.currTime
       
   def countDown(self):
@@ -44,6 +51,9 @@ class Timer:
       self.currTime = self.totalTime-self.countUp()
       return self.currTime
     
+  def setCurrTime(self,time):
+      self.currTime = time
+  
   def showTime(self, surface):
       '''Uses pygame font object to draw time on screen.
       Argument - Surface (pygame Surface) to add text to.'''
@@ -53,7 +63,7 @@ class Timer:
                    (self.fontSize*1.2, 
                     int(self.fontSize*1.2)))
 
-  def setToZero(self,time=60): 
+  def reset(self,time=60): 
       '''Gets computer clock time at method call,
       and resets Timer values (self.currTime).'''
       self.start_ticks=pygame.time.get_ticks()
@@ -63,9 +73,12 @@ class Timer:
       else:
           self.currTime = 0
           
+  
+          
 # ================ EXAMPLE USE ================
 #instantiate object
 # timer = Timer()
+
 
 # inside a loop, call methods:
 # to count down, only call: timer.countDown()
@@ -73,6 +86,14 @@ class Timer:
 # DO NOT call both countDown and countUp in the same while loop!
 
 # You can choose to show the text on screen by using:
-# timer.showTime(WIN) # pass in the name of the surface to draw on
+# timer.showTime(win) # pass in the name of the surface to draw on
+
+# You can now override the time by calling setTime:
+# timer.setTime(0) # time set to zero
+# timer.showTime(win) # zero time displayd
+
+# You can now reset the clock (or pause it)
+# timer.reset(60) # resets the countDown timer to 60 s
+# timer.showTime() # reseet time displayed
 
 # You can have the timer counting down and not show your users for an added challenge!
